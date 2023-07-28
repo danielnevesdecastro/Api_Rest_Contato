e24572aec6f89058abc2d2051b87cd57d8dbdd74
Daniel Neves <danielnevesdecastro@gmail.com>
2023-07-28 07:20:07 -0300

    Put

--- src/main/java/com/unimed/Agenda/controller/ContatoController.java
+++ src/main/java/com/unimed/Agenda/controller/ContatoController.java
@@ -2,12 +2,12 @@ package com.unimed.Agenda.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,29 +20,38 @@ import com.unimed.Agenda.repository.ContatoRepository;
 @RequestMapping("/contato")
 public class ContatoController {
 
-	@Autowired
 	private ContatoRepository contatoRepository;
-	
+
+	public ContatoController(ContatoRepository contatoRepository) {
+		this.contatoRepository = contatoRepository;
+	}
+
 	@GetMapping
 	public List<Contato> getContato() {
 		return contatoRepository.findAll();
 	}
-	
+
 	@GetMapping("/contato/{id}")
 	public Contato getContatoById(@PathVariable Long id) {
 		return contatoRepository.findById(id).get();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Contato setContato(@RequestBody Contato contato) {
 		return contatoRepository.save(contato);
 	}
-	
+
 	@DeleteMapping("/contato/{id}")
-	public void deleteContato(@PathVariable Long id ) {
+	public void deleteContato(@PathVariable Long id) {
 		contatoRepository.deleteById(id);
 	}
 	
-	
+	@PutMapping("update/{id}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public Contato updateContato(@PathVariable Long id, @RequestBody Contato contatoAtt) {
+		contatoAtt.setId(id);
+		return contatoRepository.save(contatoAtt);
+	}
+
 }
